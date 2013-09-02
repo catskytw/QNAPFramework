9d6ab740837e95cdfb443a1711a85008c7bcdd1c
Change.Liao <catskytw@gmail.com>
2013-09-02 17:38:55 +0800

    Initial Commit

---
+++ QNAPFrameworkTests/QNAPFrameworkTests.h
@@ -0,0 +1,13 @@
+//
+//  QNAPFrameworkTests.h
+//  QNAPFrameworkTests
+//
+//  Created by Change.Liao on 13/9/2.
+//  Copyright (c) 2013年 QNAP. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QNAPFrameworkTests : SenTestCase
+
+@end
---
+++ QNAPFrameworkTests/QNAPFrameworkTests.m
@@ -0,0 +1,32 @@
+//
+//  QNAPFrameworkTests.m
+//  QNAPFrameworkTests
+//
+//  Created by Change.Liao on 13/9/2.
+//  Copyright (c) 2013年 QNAP. All rights reserved.
+//
+
+#import "QNAPFrameworkTests.h"
+
+@implementation QNAPFrameworkTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in QNAPFrameworkTests");
+}
+
+@end
