02d01bfb38c1d06de238ca44e9373441a4219203
Chen-chih Liao <catskytw@gmail.com>
2013-09-03 00:54:45 +0800

    add CoreData

--- QNAPFramework/CommunicationManager/QNAPCommunicationManager.h
+++ QNAPFramework/CommunicationManager/QNAPCommunicationManager.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface QNAPCommunicationManager : NSObject
-
++ (QNAPCommunicationManager *)share;
 @end
--- QNAPFramework/CommunicationManager/QNAPCommunicationManager.m
+++ QNAPFramework/CommunicationManager/QNAPCommunicationManager.m
@@ -7,8 +7,20 @@
 //
 
 #import "QNAPCommunicationManager.h"
-
+static QNAPCommunicationManager *singletonCommunicationManager = nil;
 @implementation QNAPCommunicationManager
 
++ (QNAPCommunicationManager *)share{
+    if(singletonCommunicationManager == nil){
+        /**
+         1. create singletonCommunicationManager
+         2. binding magical record's context with afnetworking's context(which both mean the context of CoreData)
+         3. other essentail settings.
+         */
+    }
+    return singletonCommunicationManager;
+}
+
+#pragma mark - LifeCycle
 
 @end
