c599dd2765f1d6e8366d7ea6d3452e7477fefbfc
Change.Liao <catskytw@gmail.com>
2013-09-05 10:13:53 +0800

    renew .gitignore
    1. renew .gitignore
    2. add SettingInfo.h

---
+++ QNAPFramework/SettingInfo.h
@@ -0,0 +1,23 @@
+//
+//  SettingInfo.h
+//  QNAPFramework
+//
+//  Created by Change.Liao on 13/9/4.
+//  Copyright (c) 2013年 QNAP. All rights reserved.
+//
+
+#ifndef QNAPFramework_SettingInfo_h
+#define QNAPFramework_SettingInfo_h
+
+/**
+ *  Ask your admin for all information below
+ */
+#define MyCloudServerBaseURL @"http://core.api.alpha-myqnapcloud.com"
+#define CLIENT_ID @"521c609775413f6bfec8e59b"
+#define CLIENT_SECRET @"LWvRWyHFNDENTZZGCp9kcOEGed18cW02KVnV6bfrvtBL0hpu"
+#define ACCOUNT @"catskytw@gmail.com"
+#define PASSWORD @"12345678"
+
+
+
+#endif
